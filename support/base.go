88c4272c12a30f1af3e7b74bc8cc155cf96a792d
William Méndez <williamm@avalith.net>
2021-05-25 14:30:41 -0500

    Feat: Implement relative path for invoke resources
    such as chromedriver and selenium for multiplatform support

--- support/base.go
+++ support/base.go
@@ -2,19 +2,35 @@ package support
 
 import (
 	"log"
+	"os"
+	"path"
+	"runtime"
 
 	"github.com/tebeka/selenium"
 )
 
 var (
 	driver           selenium.WebDriver
-	chromeDriverPath = `C:\Users\jonat\go\src\github.com\jonathanbs9\go-selenium-avalith-web-test\resources\chromedriver.exe`
-	seleniumPath     = `C:\Users\jonat\go\src\github.com\jonathanbs9\go-selenium-avalith-web-test\resources\selenium-server-standalone-3.141.59.jar`
+	chromeDriverPath string
+	seleniumPath     string
 	port             = 4444
 )
 
 func WDInit() selenium.WebDriver {
 	var err error
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Println("Error con el directorio ", err.Error())
+	}
+
+	if runtime.GOOS == "windows" {
+		chromeDriverPath = path.Join(currentDir, "resources\\chromedriver.exe")                    //`C:\Users\jonat\go\src\github.com\jonathanbs9\go-selenium-avalith-web-test\resources\chromedriver.exe`
+		seleniumPath = path.Join(currentDir, "resources\\selenium-server-standalone-3.141.59.jar") //`C:\Users\jonat\go\src\github.com\jonathanbs9\go-selenium-avalith-web-test\resources\selenium-server-standalone-3.141.59.jar`
+	} else {
+		chromeDriverPath = path.Join(currentDir, "resources/chromedriver")
+		seleniumPath = path.Join(currentDir, "resources/selenium-server-standalone-3.141.59.jar")
+	}
 
 	ops := []selenium.ServiceOption{
 		selenium.ChromeDriver(seleniumPath),
